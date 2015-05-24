aa1e7eb4f1a1fea3d458043f93ab18e027e1b962
Gengyu Shi <univer.shi@gmail.com>
2015-05-24 14:48:22 +0100

    -

--- feeding/src/main/fetch_all_eod_quotes.py
+++ feeding/src/main/fetch_all_eod_quotes.py
@@ -5,13 +5,12 @@ Created on 23 May 2015
 '''
 
 import sys, os, inspect
-from quotes import quote_feeder
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
+print("cmd_folder = %s", cmd_folder)
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
 
 if __name__ == '__main__':
-    cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
-    print("cmd_folder = %s", cmd_folder)
-    if cmd_folder not in sys.path:
-        sys.path.insert(0, cmd_folder)
-        
+    from quotes import quote_feeder
     feeder = quote_feeder.QuoteFeeder(sys.argv[1])
     feeder.fetch_all()
