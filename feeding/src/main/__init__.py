6fed5881669dbbfbdd5a9273e7f474682cecb12f
Gengyu Shi <univer.shi@gmail.com>
2015-05-24 16:05:05 +0100

    -

--- feeding/src/main/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys, os, inspect
-
-current_file = inspect.getfile(inspect.currentframe())
-parent_dir = os.path.join(os.path.split(current_file)[0], os.path.pardir)
-parent_folder = os.path.realpath(os.path.abspath(parent_dir))
-if parent_folder not in sys.path:
-    sys.path.insert(0, parent_folder)
--- feeding/src/main/fetch_all_eod_quotes.py
+++ feeding/src/main/fetch_all_eod_quotes.py
@@ -4,7 +4,13 @@ Created on 23 May 2015
 @author: Univer
 '''
 
-import sys
+import sys, os, inspect
+
+current_file = inspect.getfile(inspect.currentframe())
+parent_dir = os.path.join(os.path.split(current_file)[0], os.path.pardir)
+parent_folder = os.path.realpath(os.path.abspath(parent_dir))
+if parent_folder not in sys.path:
+    sys.path.insert(0, parent_folder)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
